c3ba3595d25f70b3e219fbd724dce1c03836a5a6
Kenvix <kenvixzure@live.com>
2019-12-07 09:30:31 +0800

    Init

---
+++ .vscode/compileKeil.bat
@@ -0,0 +1,23 @@
+@echo off
+set KEIL_BIN_FOLDER=C:\RT\Keil\C51\BIN
+
+cls
+echo Keil Compiler batch by Kenvix
+echo Compiler path is %KEIL_BIN_FOLDER%
+
+cd /d "%1\build"
+
+call "%KEIL_BIN_FOLDER%\C51.EXE" "%2\%3.c" LARGE OPTIMIZE (8,SIZE) BROWSE OBJECTEXTEND TABS (2) 
+move "%2\%3.LST" "%2\..\build" > NUL || goto failed
+move "%2\%3.OBJ" "%2\..\build" > NUL || goto failed
+
+call "%KEIL_BIN_FOLDER%\BL51.EXE" "%1\build\%3.OBJ" TO %3 
+call "%KEIL_BIN_FOLDER%\OH51.EXE" "%1\build\%3" || goto failed
+
+echo Compiled successfully: build\%3.hex
+exit 0
+
+:failed
+set ERRCODE=%errorlevel%
+echo Compilation Failed: Code %ERRCODE%
+exit %ERRCODE%
